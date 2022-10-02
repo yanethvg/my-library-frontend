7256f19f14067d02a0826a099f9f16babb7ed95f
yanethvg <zoilavillatoro6694@gmail.com>
2022-10-02 00:25:38 -0600

    added text in home

--- src/pages/CheckinPage.jsx
+++ src/pages/CheckinPage.jsx
@@ -73,62 +73,64 @@ function CheckinPage() {
     <>
       <div>
         <h1 className="font-bold text-2xl text-blue-900 my-6 text-center">
-         Books 
+          Books List
         </h1>
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-              Title
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-last-name"
-              type="text"
-              placeholder="Enter Title"
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </div>
-          <div className="w-full md:w-1/3 px-3">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-              Author
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-last-name"
-              type="text"
-              placeholder="Enter Author"
-              onChange={(e) => setAuthor(e.target.value)}
-            />
-          </div>
-          <div className="w-full md:w-1/3 px-3">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-              Genre
-            </label>
-            <div className="relative">
-              <select
-                className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-state"
-                onChange={(e) => setGenre(e.target.value)}
-              >
-                <option value="">Choose Genre</option>
-                {genres.map((genre) => (
-                  <option key={genre.id} value={genre.id}>
-                    {genre.name}
-                  </option>
-                ))}
-              </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg
-                  className="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
+        {books.length > 0 && (
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                Title
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-last-name"
+                type="text"
+                placeholder="Enter Title"
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            <div className="w-full md:w-1/3 px-3">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                Author
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-last-name"
+                type="text"
+                placeholder="Enter Author"
+                onChange={(e) => setAuthor(e.target.value)}
+              />
+            </div>
+            <div className="w-full md:w-1/3 px-3">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                Genre
+              </label>
+              <div className="relative">
+                <select
+                  className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="grid-state"
+                  onChange={(e) => setGenre(e.target.value)}
                 >
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
+                  <option value="">Choose Genre</option>
+                  {genres.map((genre) => (
+                    <option key={genre.id} value={genre.id}>
+                      {genre.name}
+                    </option>
+                  ))}
+                </select>
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                  <svg
+                    className="fill-current h-4 w-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                  </svg>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        )}
         {inTransition && !loading ? (
           <div className="d-flex justify-content-center">
             <Loading type={"spin"} color={"#0000ff"} />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,21 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
 function HomePage() {
+  const auth = useSelector((state) => state.auth.access);
   return (
     <>
       <div className="max-w-screen-lg bg-indigo-500 shadow-2xl rounded-lg mx-auto text-center py-12 mt-4">
         <h2 className="text-3xl leading-9 font-bold tracking-tight text-white sm:text-4xl sm:leading-10">
-          Start studying with us
+          Welcome to the Library
         </h2>
         <div className="mt-8 flex justify-center">
-          <div className="inline-flex rounded-md bg-white shadow">
-            <a href="#" className="text-gray-700 font-bold py-2 px-6">
-              Start
-            </a>
-          </div>
+          <p>Have a Good Day {auth.user.full_name}</p>
+         
         </div>
       </div>
     </>
   );
 }
 
-export default HomePage ;
+export default HomePage;
