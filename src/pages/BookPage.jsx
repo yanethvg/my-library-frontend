b990ad2776ae7788db31feb093d01a853dfb41a1
yanethvg <zoilavillatoro6694@gmail.com>
2022-10-01 18:41:00 -0600

    added genreAction for getting all genres

--- src/pages/BookPage.jsx
+++ src/pages/BookPage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useCallback } from "react";
 //redux
 import { useDispatch, useSelector } from "react-redux";
 import { getBooksAction } from "../actions/book/getBooksAction";
+import { getGenresAction} from "../actions/genre/getGenresAction";
 //components
 import Books from "../components/book/Books";
 import CustomPagination from "../components/basic/CustomPagination";
@@ -19,6 +20,7 @@ function BookPage() {
   const books = useSelector((state) => state.books.books);
   const total = useSelector((state) => state.books.pages);
   const loading = useSelector((state) => state.books.loading);
+  const genres = useSelector((state) => state.genres.genres);
 
   const handleChangePage = useCallback((page) => {
     setPage(page);
@@ -28,8 +30,13 @@ function BookPage() {
     dispatch(getBooksAction(auth.access_token, page, title, author, genre));
   };
 
+  const loadGenres = () => {
+    dispatch(getGenresAction(auth.access_token));
+  };
+
   useEffect(() => {
     load(page, title, author, genre);
+    loadGenres();
   }, [page, title, author, genre, auth.access_token]);
 
   return (
@@ -38,6 +45,69 @@ function BookPage() {
         <h1 className="font-bold text-2xl text-blue-900 my-6 text-center">
           Books
         </h1>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            >
+              Title
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-last-name"
+              type="text"
+              placeholder="Enter Title"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            {/* <p class="text-red-500 text-xs italic">
+              Please fill out this field.
+            </p> */}
+          </div>
+          <div className="w-full md:w-1/3 px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            >
+              Author
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-last-name"
+              type="text"
+              placeholder="Enter Author"
+              onChange={(e) => setAuthor(e.target.value)}
+            />
+          </div>
+          <div className="w-full md:w-1/3 px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            >
+             Genre
+            </label>
+            <div className="relative">
+              <select
+                className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-state"
+                onChange={(e) => setGenre(e.target.value)}
+              >
+                <option value="">Choose Genre</option>
+                {genres.map((genre) => (
+                  <option key={genre.id} value={genre.id}>
+                    {genre.name}
+                  </option>
+                ))}
+              </select>
+              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <svg
+                  className="fill-current h-4 w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
         <Books books={books} />
         {total > 1 && loading == false && (
           <div className="d-flex justify-content-center">
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import LoginReducer from "./LoginReducer";
 import BookReducer from "./BookReducer";
+import GenreReducer from "./GenreReducer";
 
 export default combineReducers({
   auth: LoginReducer,
   books: BookReducer,
+  genres: GenreReducer,
 });
